2aabf11f43bfebda477cba0e0474a94ffe8b9886
sharon-raz9 <103305402+sharon-raz9@users.noreply.github.com>
2022-04-09 23:06:49 +0300

    Update attack-login-pages.py

--- attack-login-pages.py
+++ attack-login-pages.py
@@ -5,14 +5,14 @@ import subdomain_scanner
 
 # I would run on the domains with the help of the library asyncio
 
-# extract the domains list
-domains_list = None
-if "-d" in sys.argv:
-    domains_list = [domain for domain in sys.argv[sys.argv.index('-d') + 1:]]
-    domains_list = [domain.replace(',', '') for domain in domains_list]
-
-
 if __name__ == '__main__':
+    
+    # extract the domains list
+    domains_list = None
+    if "-d" in sys.argv:
+        domains_list = [domain for domain in sys.argv[sys.argv.index('-d') + 1:]]
+        domains_list = [domain.replace(',', '') for domain in domains_list]
+
     for domain in domains_list:
         print(f"Current domain is: {domain}")
         p = multiprocessing.Process(target=subdomain_scanner.SubDomainScanner(domain).scan)
